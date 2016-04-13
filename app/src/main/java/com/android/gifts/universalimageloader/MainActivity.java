1cb250d3cd38edbaad8a62e1fa3b446cb547cb23
Android Gifts <info@androidgifts.com>
2016-04-13 17:54:43 +0200

    initial commit

---
+++ app/src/main/java/com/android/gifts/universalimageloader/MainActivity.java
@@ -0,0 +1,22 @@
+package com.android.gifts.universalimageloader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(this)
+        .build();
+
+        ImageLoader imageLoader = ImageLoader.getInstance();
+        imageLoader.init(config);
+    }
+}
